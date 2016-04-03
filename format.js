912fb6db240ed95e1b7193322f2bde9efe54175b
Bella Shim <ks3312@columbia.edu>
2016-04-03 06:32:07 -0400

    Parsed through completed courses and appended them to the table. Added formatting as well

--- format.js
+++ format.js
@@ -3,55 +3,18 @@ var DOCUMENT = 9;
 var DOCUMENT_FRAGMENT = 11;
 var TEXT = 3;
 
-// Enter things that you\'d like to replace
-// var MATCH = [\'computer\',\'person\'];
-// var REPLACE = [\'robot\',\'cantaloupe\'];
-
-// walk(document.body);
-
-// function walk(node) {
-//     // Function from here for replacing text: http://is.gd/mwZp7E
-
-//     var child, next;
-
-//     switch (node.nodeType) {
-//         case ELEMENT:  // Element
-//         case DOCUMENT:  // Document
-//         case DOCUMENT_FRAGMENT: // Document fragment
-//             child = node.firstChild;
-//             while (child) {
-//                 next = child.nextSibling;
-//                 walk(child);
-//                 child = next;
-//             }
-//             break;
-
-//         case TEXT: // Text node
-//             replaceText(node);
-//             break;
-//     }
-// }
-
-// function replaceText(textNode) {
-//     var v = textNode.nodeValue;
-
-//     // Go through and match/replace all the strings we\'ve given it, using RegExp.
-//     for (var i = 0; i < MATCH.length; i++) {
-//         v = v.replace(new RegExp(\'\\b\' + MATCH[i] + \'\\b\', \'g\'), REPLACE[i]);
-//     }
-
-//     textNode.nodeValue = v;
-// }
 window.onload = function () {
     var preText = document.getElementsByTagName("pre")[0];
-    var textString = preText.innerHTML;
-    var lineArray = textString.split("\n");
+    // var textString = preText.innerHTML;
+    // var lineArray = textString.split("\n");
 
-    var newText = ""
+    // var newText = ""
 
-    for (i = lineArray.length-1; i >= 0; i--){
-        newText += lineArray[i] + "\n"
-    }
+    // for (i = lineArray.length-1; i >= 0; i--){
+    //     newText += lineArray[i] + "\n"
+    // }
+
+    var rawText = document.getElementsByTagName("pre")[0].innerHTML;
 
     //Replacing <pre> area with white space
     document.getElementsByTagName("pre")[0].innerHTML = " ";
@@ -71,27 +34,7 @@ window.onload = function () {
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> \
     <link rel="stylesheet" type="text/css" href="css/style.css"> \
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> \
-    <!-- link style --> \
-    <script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');ga(\'create\', \'UA-50221324-4\', \'auto\'); ga(\'send\', \'pageview\');</script> \
-  	</head> \
-	<center> <h1> Degree Audit </h1> </center> \
-    <section> \
-      <table class="table table-bordered"> \
-        <thead> \
-          <tr> \
-            <!-- edited bootstrap.css #2264--> \
-            <th colspan="7"> 9 ways of knowing </th> \
-          </tr> \
-          <tr> \
-            <th>Requirement</th> \
-            <th>Department</th> \
-            <th>Course Number</th> \
-            <th>Course Title</th> \
-            <th>Credit</th> \
-            <th>Grade</th> \
-            <th>Status</th> \
-          </tr> \
-          <style> \
+    <style> \
             html, body \
       		{ \
         	font-family: Raleway; \
@@ -113,7 +56,7 @@ window.onload = function () {
 		    { \
 		      background-color: #FFF; \
 		    } \
-		    footer \
+            footer \
 		    { \
 		      font-size: 1rem; \
 		      color: #4c4c4c; \
@@ -123,312 +66,35 @@ window.onload = function () {
 		      text-align: center; \
 		    } \
     </style> \
+    <!-- link style --> \
+    <script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');ga(\'create\', \'UA-50221324-4\', \'auto\'); ga(\'send\', \'pageview\');</script> \
+  	</head> \
+	<center> <h1> Degree Audit </h1> </center> \
+    <section> \
+      //List of Completed Courses \
+      <table class="table table-bordered"> \
+        <thead> \
+          <tr> \
+            <!-- edited bootstrap.css #2264--> \
+            <th colspan="8"> List of Completed Courses </th> \
+          </tr> \
+          <tr> \
+            <th>Course Title</th> \
+            <th>Semester</th> \
+            <th>Department</th> \
+            <th>Course Number</th> \
+            <th>Section</th> \
+            <th>Credit</th> \
+            <th>Grade</th> \
+            <th>Status</th> \
+          </tr> \
+         \
         </thead> \
-        <tbody> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
+        <tbody id="courseTable"> \
         </tbody> \
       </table> \
     </section> \
-    <br> \
-    <section> \
-      <table class="table table-bordered"> \
-        <thead> \
-          <tr> \
-            <!-- edited bootstrap.css #2264--> \
-            <th colspan="7"> Major Requirements </th> \
-          </tr> \
-          <tr> \
-            <th>Requirement</th> \
-            <th>Department</th> \
-            <th>Course Number</th> \
-            <th>Course Title</th> \
-            <th>Credit</th> \
-            <th>Grade</th> \
-            <th>Status</th> \
-          </tr> \
-        </thead> \
-        <tbody> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-        </tbody> \
-      </table> \
-    </section> \
- \
-    <br> \
- \
-    <section> \
-      <table class="table table-bordered"> \
-        <thead> \
-          <tr> \
-            <!-- edited bootstrap.css #2264--> \
-            <th colspan="7"> Minor Requirements </th> \
-          </tr> \
-          <tr> \
-            <th>Requirement</th> \
-            <th>Department</th> \
-            <th>Course Number</th> \
-            <th>Course Title</th> \
-            <th>Credit</th> \
-            <th>Grade</th> \
-            <th>Status</th> \
-          </tr> \
-        </thead> \
-        <tbody> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-        </tbody> \
-      </table> \
-    </section>     \
- \
-    <br> \
- \
-    <section> \
-      <table class="table table-bordered"> \
-        <thead> \
-          <tr> \
-            <!-- edited bootstrap.css #2264--> \
-            <th colspan="7"> Transfer/ AP Credits </th> \
-          </tr> \
-          <tr> \
-            <th>Requirement</th> \
-            <th>Department</th> \
-            <th>Course Number</th> \
-            <th>Course Title</th> \
-            <th>Credit</th> \
-            <th>Grade</th> \
-            <th>Status</th> \
-          </tr> \
-        </thead> \
-        <tbody> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td></td> \
-          </tr> \
-        </tbody> \
-      </table> \
-    </section> \
- \
-    <br> \
- \
-    <section> \
-      <table class="table table-bordered"> \
-        <thead> \
-          <tr> \
-            <!-- edited bootstrap.css #2264--> \
-            <th colspan="7">List of Completed Courses</th> \
-          </tr> \
-          <tr> \
-            <th>Requirement</th> \
-            <th>Department</th> \
-            <th>Course Number</th> \
-            <th>Course Title</th> \
-            <th>Credit</th> \
-            <th>Grade</th> \
-            <th>Status</th> \
-          </tr> \
-        </thead> \
-        <tbody> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-          <tr> \
-            <td>First Year English</td> \
-            <td>ENGL</td> \
-            <td>BC1201</td> \
-            <td>Legacy of the Med.</td> \
-            <td>3</td> \
-            <td>A+</td> \
-            <td><center><img src=http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png width="20" height="20"></center></td> \
-          </tr> \
-        </tbody> \
-      </table> \
-    </section>'; // HTML string
+    <br> \ '; // HTML string
 
 	var div = document.createElement('div');
 	div.innerHTML = s;
@@ -436,5 +102,72 @@ window.onload = function () {
 	var replace = document.getElementById("Content");
 	replace.appendChild(div);
 
+	var audit = rawText.split(`NO     *****         SUMMARY OF DEGREE REQUIREMENTS         *****`)
+	var summary = audit[1].split('\n                                                                 \n')
+	var courseList = summary[1].split('\n').map(function(x){return x.trim()}).map(x => x.split("   ")).filter(x => x.length === 2)
 
+	for (i = 0; i < courseList.length; i++) { 
+        var courseRow = document.createElement('tr');
+        var details = courseList[i][0].split(' ');
+
+        var courseTitle = courseList[i][1];
+        var courseTitleData = document.createElement('td');
+        var courseTitleText = document.createTextNode(courseTitle);
+        courseTitleData.appendChild(courseTitleText);
+        courseRow.appendChild(courseTitleData);
+	    
+        var semester = details[0];
+        var semesterData = document.createElement('td');
+        var semesterText = document.createTextNode(semester);
+        semesterData.appendChild(semesterText);
+        courseRow.appendChild(semesterData);
+	    
+        var department = details[1];
+        var departmentData = document.createElement('td');
+        var departmentText = document.createTextNode(department);
+        departmentData.appendChild(departmentText);
+        courseRow.appendChild(departmentData);
+
+	    var courseNumber = details[2];
+        var courseNumberData = document.createElement('td');
+        var courseNumberText = document.createTextNode(courseNumber);
+        courseNumberData.appendChild(courseNumberText);
+        courseRow.appendChild(courseNumberData);
+
+	    var section = details[4];
+        var sectionData = document.createElement('td');
+        var sectionText = document.createTextNode(section);
+        sectionData.appendChild(sectionText);
+        courseRow.appendChild(sectionData);
+
+        //had to skip index 5, because it's whitespace there
+
+	    var credit = details[6];
+        var creditData = document.createElement('td');
+        var creditText = document.createTextNode(credit);
+        creditData.appendChild(creditText);
+        courseRow.appendChild(creditData);
+
+	    var grade = details[7];
+        var gradeData = document.createElement('td');
+        var gradeText = document.createTextNode(grade);
+        gradeData.appendChild(gradeText);
+        courseRow.appendChild(gradeData);
+
+        var statusData = document.createElement('td');
+        if (grade != 'IP') {
+            var statusImg = document.createElement('img');
+            statusImg.src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
+            statusImg.setAttribute('width','20');
+            statusImg.setAttribute('height','20');
+            statusData.appendChild(statusImg);
+        } else {
+            var statusEmpty = document.createTextNode(" ");
+            statusData.appendChild(statusEmpty);
+        }
+        courseRow.appendChild(statusData);
+
+        document.getElementById('courseTable').appendChild(courseRow);
+
+	}
 }
