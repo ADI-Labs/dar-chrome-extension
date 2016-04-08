f246e513fe502ca2a5e77e5fec9f249b33f24815
joshuazweig <jmz2135@columbia.edu>
2016-04-08 13:20:30 -0400

    Remove space in URL to CSS

--- format.js
+++ format.js
@@ -27,7 +27,7 @@ window.onload = function () {
     <meta name="description" content="Degree Audit Report"> \
     <meta name="creator" content="ADI Labs, Degree Audit Report Team"> \
     <link href=\'https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900\' rel=\'stylesheet\' type=\'text/css\'> \
-    <link rel="stylesheet" href="https:/ /cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"> \
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"> \
     <!-- style import --> \
     <!--<link rel="stylesheet" type="text/css" href="css/reset.css"> \
     <link rel="stylesheet" type="text/css" href="css/style.css">--> \
